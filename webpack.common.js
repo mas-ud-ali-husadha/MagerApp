98c2a569bb2853d6a79836063e90a98786f589b4
Mas'ud Ali Husada <lightning300402@gmail.com>
2021-06-24 19:53:12 +0700

    ok

--- webpack.common.js
+++ webpack.common.js
@@ -27,7 +27,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/,
         use: [
           'file-loader',
         ],
